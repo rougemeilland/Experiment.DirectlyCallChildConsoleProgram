e8dddd038a867f79995ed60eb978c2d0cba2f67f
rougemeilland <28302784+rougemeilland@users.noreply.github.com>
2023-03-04 16:59:48 +0900

    Initial build

---
+++ ChildProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ChildProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"I am the child program. (process-id: {Process.GetCurrentProcess().Id})");
+            Console.WriteLine($"args: [{string.Join(", ", args.Select(arg => $"\"{arg.Replace(@"\", @"\\").Replace(@"""", @"\""")}\""))}]");
+        }
+    }
+}
---
+++ ParentProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ParentProgram
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"I am the parent program. (process-id: {Process.GetCurrentProcess().Id})");
+            Console.WriteLine("----------  start of child program  ----------");
+            ChildProgram.Program.Main(new[] { "first", "second", "third" });
+            Console.WriteLine("----------  end of child program  ----------");
+            Console.Beep();
+            Console.WriteLine("completed");
+            _ = Console.ReadLine();
+        }
+    }
+}
